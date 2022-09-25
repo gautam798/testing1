3bbb6fd94b8840e15334f146e8bf5059bd61d801
Gautam <gautam>
2022-09-25 21:54:46 +0530

    test1

---
+++ hunter-wordlist.py
@@ -0,0 +1,11 @@
+#this is a script where you will be suggested to to use wordlist based on tech,language,other headers details
+
+input_data = ["go","php","aspx"]
+a=1
+def suggest(a):
+    if a == "go":
+         pass
+         #wordlist 1
+    elif a == "":
+        pass
+
